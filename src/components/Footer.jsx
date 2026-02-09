54b9f31458db628a5249506a53f37e5c0bc24a52
Dani-Alarcon <daniel.alarcon.7e6@itb.cat>
2026-02-09 19:23:13 +0100

    footer con scss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import "../assets/footer.scss";
 
 class Footer extends React.Component {
-
     render() {
         return (
-                <footer class="bg-[#2c373d] w-full fixed bottom-0 z-50 border-t-4 border-teal-400">
-                    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 text-center">
-                        <p class="text-gray-400 text-sm">© Gaming 2025</p>
-                    </div>
-                </footer>    
+            <footer className="site-footer">
+                <div className="footer-container">
+                    <p className="footer-copy">© Gaming 2025</p>
+                </div>
+            </footer>    
         );
     }
 }
